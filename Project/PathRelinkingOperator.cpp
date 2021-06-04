35848d60399dc6a2914394f46c0bfb5441f197b3
Tiago Funk <tiagoff.tf@gmail.com>
2021-06-04 14:31:02 -0300

    Fixed bug of writing over different sized routes in path relinking

--- Project/PathRelinkingOperator.cpp
+++ Project/PathRelinkingOperator.cpp
@@ -10,15 +10,21 @@ Solution * PathRelinkingOperator::execute( Solution * start, Solution * end ){
     Solution * actual = new Solution( *start );
     Solution * best = new Solution( *start );
 
-    int a = end->get_number_paths();
-    int b = -1;
-    for( int i = 0; i < a; i++){
-        b = end->get_length_of_path( i );
-        for( int j = 1; j < b-1; j++){
-            if( actual->rewrite_vertice( i, j, end->get_vertice_in_path( i, j ) ) ){
-                if( best->get_total_rewards() < actual->get_total_rewards() ){
-                    delete best;
-                    best = new Solution( *actual );
+    for( int i = 0; i < end->get_number_paths(); i++){
+        for( int j = 1; j < end->get_length_of_path( i )-1; j++){
+            if( j < actual->get_length_of_path( i )-1 ){
+                if( actual->rewrite_vertice( i, j, end->get_vertice_in_path( i, j ) ) ){
+                    if( best->get_total_rewards() < actual->get_total_rewards() ){
+                        delete best;
+                        best = new Solution( *actual );
+                    }
+                }
+            }else{
+                if( actual->add_vertice_in_position( i, j, end->get_vertice_in_path( i, j ) ) ){
+                    if( best->get_total_rewards() < actual->get_total_rewards() ){
+                        delete best;
+                        best = new Solution( *actual );
+                    }
                 }
             }
         }
