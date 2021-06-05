6241efb813333614f2036e049e0291e2f80ca623
Tiago Funk <tiagoff.tf@gmail.com>
2021-06-04 23:08:38 -0300

    Alpha parameter was read as an integer and the correct one would be double

--- Project/main.cpp
+++ Project/main.cpp
@@ -15,7 +15,7 @@ int main( int argc, char * argv[] ){
     int seed = stoi( argv[ 3 ] );
 	ArgumentReader arg( argc, argv );
     string file = arg.getValue("--file");
-    double alpha = stoi( arg.getValue("--alpha") );
+    double alpha = stod( arg.getValue("--alpha") );
     int iterations = stoi( arg.getValue("--iterations") );
     bool path = arg.getValue("--path") == "y"? true : false; 
 
@@ -24,7 +24,7 @@ int main( int argc, char * argv[] ){
 
     GRASP g( iterations, seed, alpha, new LocalSearchWithOperators(), new PathRelinkingOperator( path ), &i );
     Solution * s = g.execute();
-    cout << s->get_total_rewards() << endl;
+    cout << -1*s->get_total_rewards() << endl;
     //cout << s->to_string() << endl;
     return 0;
 }
