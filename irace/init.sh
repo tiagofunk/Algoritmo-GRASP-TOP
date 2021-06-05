4c7f56562341771c638d0738a0df36c5ba166421
Tiago Funk <tiagoff.tf@gmail.com>
2021-06-05 18:48:51 -0300

    experiment in irace

---
+++ irace/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export IRACE_HOME=/home/tiago/R/x86_64-pc-linux-gnu-library/3.4/irace
+export PATH=${IRACE_HOME}/bin/:$PATH
+
+cd ..
+cd Project
+
+make
+
+cd ..
+cd irace
+
+rm target-runner
+
+cp "../Project/TOP_GRASP_TS_PR" "./target-runner"
+
+irace --parallel 8
+
