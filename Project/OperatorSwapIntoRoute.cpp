2add9320ccf8cf597cc3ac9894f1df9848a559d7
Tiago Funk <tiagoff.tf@gmail.com>
2021-06-19 01:22:35 -0300

    Each route that receives the upgrade is now saved

--- Project/OperatorSwapIntoRoute.cpp
+++ Project/OperatorSwapIntoRoute.cpp
@@ -3,10 +3,12 @@
 Solution * OperatorSwapIntoRoute::execute( Solution * s, vector< Vertice * > unused_vertices ){
     Solution * best = new Solution( *s );
     Solution * actual = new Solution( *s );
+    Solution * backup = 0;
     this->unused_vertices = unused_vertices;
 
     for( int i = 0; i < s->get_number_paths(); i++){
         for (int j = 1; j < s->get_length_of_path( i )-1; j++){
+            backup = new Solution( *actual );
             for(int k = j+1; k < s->get_length_of_path( i )-1; k++ ){
                 actual->swap( i, j, k );
                 if( actual->get_time_path( i ) < best->get_time_path( i ) ){
@@ -14,8 +16,9 @@ Solution * OperatorSwapIntoRoute::execute( Solution * s, vector< Vertice * > unu
                     best = new Solution( *actual );
                 }
                 delete actual;
-                actual = new Solution( *s );
+                actual = new Solution( *backup );
             }
+            delete backup;
         }
     }
     delete s;
