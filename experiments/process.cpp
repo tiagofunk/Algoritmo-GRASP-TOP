89bb37a30390727ba55721496c3c3eb72fdb7fa2
Tiago Funk <tiagoff.tf@gmail.com>
2021-06-24 10:59:20 -0300

    results compilation

--- experiments/process.cpp
+++ experiments/process.cpp
@@ -12,16 +12,24 @@ struct instance_value{
     string file;
     int position;
     int results[ NUMBER_RESULTS ];
-    double mean;
-    double stand_desv;
+    int time[ NUMBER_RESULTS ];
+    int best_result;
+    double mean_results;
+    double stand_desv_results;
+    double mean_time;
+    int state_of_art;
 };
 
-vector< instance_value > add_value_on_instance( vector< instance_value > instances, string instance, int value ){
+vector< instance_value > add_value_on_instance( vector< instance_value > instances, string instance, int value, int time ){
     bool finded = false;
     for (unsigned int i = 0; i < instances.size(); i++){
         if( instances[ i ].file == instance ){
             instances[ i ].results[ instances[ i ].position ] = value;
+            instances[ i ].time[ instances[ i ].position ] = time;
             instances[ i ].position++;
+            if( instances[ i ].best_result < value ){
+                instances[ i ].best_result = value;
+            }
             finded = true;
         }
     }
@@ -30,6 +38,8 @@ vector< instance_value > add_value_on_instance( vector< instance_value > instanc
         inst.file = instance;
         inst.position = 1;
         inst.results[ 0 ] = value;
+        inst.time[ 0 ] = time;
+        inst.best_result = value;
         instances.push_back( inst );
     }
     return instances;
@@ -38,6 +48,7 @@ vector< instance_value > add_value_on_instance( vector< instance_value > instanc
 vector< instance_value > read_file( string file ){
     string instance_name;
     int result, time;
+    int i, f;
     vector< instance_value > instances;
 
     ifstream reader( file.c_str(), ios::in );
@@ -47,22 +58,49 @@ vector< instance_value > read_file( string file ){
 
     while( !reader.eof() ){
         reader >> instance_name >> result >> time;
-        //cout << instance_name << " " << result << " " << time << endl;
-        instances = add_value_on_instance( instances, instance_name, result );
+        i = instance_name.find_last_of("/") + 1;
+        f = instance_name.find_last_of(".") - i;
+        instance_name = instance_name.substr( i, f );
+        instances = add_value_on_instance( instances, instance_name, result, time );
+    }
+    instances.erase( instances.end()-1 );
+    reader.close();
+    return instances;
+}
+
+vector< instance_value > read_file_state_of_art( string file, vector< instance_value > instances ){
+    string instance_name;
+    int result;
+
+    ifstream reader( file.c_str(), ios::in );
+    if( !reader ){
+        throw runtime_error( "Erro on open file: " + file );
     }
 
+    while( !reader.eof() ){
+        reader >> instance_name >> result;
+        for( unsigned int i = 0; i < instances.size(); i++ ){
+            if( instances[ i ].file == instance_name ){
+                instances[ i ].state_of_art = result;
+                break;
+            }
+        }
+    }
     reader.close();
     return instances;
 }
 
 vector< instance_value > calcule_mean( vector< instance_value > instances ){
-    double sum;
+    double sum_result, sum_time;
     for( unsigned int i = 0; i < instances.size(); i++ ){
-        sum = 0.0;
+        sum_result = 0.0;
+        sum_time = 0.0;
         for( int j = 0; j < instances[ i ].position; j++ ){
-            sum += instances[ i ].results[ j ];
+            sum_result += instances[ i ].results[ j ];
+            sum_time += instances[ i ].time[ j ];
         }
-        instances[ i ].mean = sum / instances[ i ].position;
+        instances[ i ].mean_results = sum_result / instances[ i ].position;
+        instances[ i ].mean_time = sum_time / instances[ i ].position;
     }
     return instances;
 }
@@ -71,42 +109,31 @@ vector< instance_value > calcule_stand_desv( vector< instance_value > instances 
     double sum, mean;
     for( unsigned int i = 0; i < instances.size(); i++ ){
         sum = 0.0;
-        mean = instances[ i ].mean;
+        mean = instances[ i ].mean_results;
         for( int j = 0; j < instances[ i ].position; j++ ){
             sum += pow( instances[ i ].results[ j ] - mean, 2);
         }
-        instances[ i ].stand_desv = sqrt( sum / instances[ i ].position );
+        instances[ i ].stand_desv_results = sqrt( sum / instances[ i ].position );
     }
     return instances;
 }
 
-void print( vector< instance_value > instances ){
-    for( unsigned int i = 0; i < instances.size(); i++ ){
-        cout << "instance: " << instances[ i ].file << endl;
-        cout << "position: " << instances[ i ].position << endl;
-        for( int j = 0; j < instances[ i ].position; j++ ){
-            cout << instances[ i ].results[ j ] << " ";
-        }
-        cout << endl;
-        cout << "mean: " << instances[ i ].mean << endl;
-        cout << "stand desv: " << instances[ i ].stand_desv << endl;
-        cout << endl << endl;
-    }
-}
-
 void print_to_csv( vector< instance_value > instances ){
-    cout << "Instância;média;desvio padrão;" << endl;
+    cout << "Instância;estado da arte;melhor;média;desvio padrão;tempo (ms)" << endl;
     for( unsigned int i = 0; i < instances.size(); i++ ){
         cout << instances[ i ].file << ";";
-        cout << instances[ i ].mean << ";";
-        cout << instances[ i ].stand_desv << ";";
+        cout << instances[ i ].state_of_art << ";";
+        cout << instances[ i ].best_result << ";";
+        cout << instances[ i ].mean_results << ";";
+        cout << instances[ i ].stand_desv_results << ";";
+        cout << instances[ i ].mean_time << ";";
         cout << endl;
     }
 }
 
 int main( int argc, char* argv[] ){
-    string file = "log.txt";
-    vector< instance_value > instances = read_file( file );
+    vector< instance_value > instances = read_file( "log.txt" );
+    instances = read_file_state_of_art( "soa.txt", instances );
     instances = calcule_mean( instances );
     instances = calcule_stand_desv( instances );
     print_to_csv( instances );
