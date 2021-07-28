ebd1042c08d048c2032525b76b5b2d2da3b3f3be
Tiago Funk <tiagoff.tf@gmail.com>
2021-07-28 13:09:30 -0300

    Pequena otimização no executor dos operadores de busca local

--- Project/LocalSearchWithOperators.cpp
+++ Project/LocalSearchWithOperators.cpp
@@ -27,9 +27,11 @@ Solution * LocalSearchWithOperators::execute( Solution * s, vector< Vertice * > 
             is_moved = true;
         }
 
-        is_bigger = false;
-        for( int i = 0; i < best->get_number_paths(); i++ ){
-            is_bigger = is_bigger || best->get_time_path( i ) > best->get_time_per_path();
+        if( !is_moved ){
+            is_bigger = false;
+            for( int i = 0; i < best->get_number_paths(); i++ ){
+                is_bigger = is_bigger || best->get_time_path( i ) > best->get_time_per_path();
+            }
         }
     }while( is_moved || is_bigger );
     delete actual;
