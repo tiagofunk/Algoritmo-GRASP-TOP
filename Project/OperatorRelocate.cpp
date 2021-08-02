c299e2ea9e8863b36a1334b8b2aa26d66ec1119f
Tiago Funk <tiagoff.tf@gmail.com>
2021-08-02 15:07:07 -0300

    Mudanças no operador de Relocate

--- Project/OperatorRelocate.cpp
+++ Project/OperatorRelocate.cpp
@@ -1,10 +1,10 @@
 #include "OperatorRelocate.h"
+#include "main.h"
 
-Solution * OperatorRelocate::execute( Solution * s, vector< Vertice * > unused_vertices ){
-    Solution * actual = new Solution( *s );
-    Solution * best   = new Solution( *s );
-    this->unused_vertices = unused_vertices;
-
+Solution * OperatorRelocate::realize_operation( Solution * sol ){
+    this->is_swaped = false;
+    Solution * actual = new Solution( *sol );
+    Solution * best   = new Solution( *sol );
     for( int i = 0; i < actual->get_number_paths(); i++ ){
         for( int j = 1; j < actual->get_length_of_path( i ) - 1; j++ ){
             for( int k = 0; k < actual->get_number_paths(); k++ ){
@@ -14,14 +14,26 @@ Solution * OperatorRelocate::execute( Solution * s, vector< Vertice * > unused_v
                         if( actual->get_total_time() < best->get_total_time() ){
                             delete best;
                             best = new Solution( *actual );
+                            this->is_swaped = true;
                         }
                         delete actual;
-                        actual = new Solution( *s );
+                        actual = new Solution( *sol );
                     }
                 }
             }    
         }
     }
-    delete s;
+    delete sol;
     return best;
+}
+
+Solution * OperatorRelocate::execute( Solution * s, vector< Vertice * > unused_vertices ){
+    this->unused_vertices = unused_vertices;
+    int count = 0;
+    do{
+        s = this->realize_operation( s );
+        count++;
+    }while( this->is_swaped );
+    show_log( "number of relocates: " + std::to_string( count ) + "\n", 3 );
+    return s;
 }
--- Project/OperatorRelocate.h
+++ Project/OperatorRelocate.h
@@ -5,7 +5,8 @@
 
 class OperatorRelocate : public Operator{
     private:
-    
+        bool is_swaped;
+        Solution * realize_operation( Solution * sol );
     public:
         virtual Solution * execute( Solution * sol, vector< Vertice * > unused_vertices );
 
