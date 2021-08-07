2f5586bf7a4116416451cfed3fb1f12336ec10e7
Tiago Funk <tiagoff.tf@gmail.com>
2021-08-06 22:20:28 -0300

    changes on tests

--- Project/tests/test_solution.h
+++ Project/tests/test_solution.h
@@ -13,115 +13,120 @@ void test_teardown(void) {
 }
 
 MU_TEST(solution_init) {
-    Solution * sol = new Solution( 2, 7.5 );
-	mu_check( sol->get_number_paths() == 2 );
-    mu_check( sol->get_time_per_path() == 7.5 );
-    mu_check( sol->get_total_rewards() == 0.0 );
-    mu_check( sol->get_total_time() == 0.0 );
-    delete sol;
+    Solution sol( 2, 7.5 );
+	mu_check( sol.get_number_paths() == 2 );
+    mu_check( sol.get_time_per_path() == 7.5 );
+    mu_check( sol.get_total_rewards() == 0.0 );
+    mu_check( absolute( sol.get_total_time(), 2 ) == absolute( 0.0, 2 ) );
+    mu_check( sol.get_rewards( 0 ) == 0 );
+    mu_check( sol.get_rewards( 1 ) == 0 );
+    mu_check( absolute( sol.get_time_path( 0 ), 2 ) == absolute( 0.0, 2 ) );
+    mu_check( absolute( sol.get_time_path( 1 ), 2 ) == absolute( 0.0, 2 ) );
 }
 
 MU_TEST(solution_set_initial_and_final_vertice) {
-    Vertice * initial = new Vertice( 0.0, 0.0, 0 );
-    Vertice * final = new Vertice( 3.0, 4.0, 0 );
-    Solution * sol = new Solution( 2, 7.5 );
+    Vertice initial( 0.0, 0.0, 0 );
+    Vertice final( 3.0, 4.0, 0 );
+    Solution sol( 2, 7.5 );
     for( int i = 0; i < 2; i++ ){
-        bool result = sol->add_initial_and_final_vertice( i, initial, final );
+        bool result = sol.add_initial_and_final_vertice( i, &initial, &final );
         mu_check( result == true );
     }
     for( int i = 0; i < 2; i++ ){
-        mu_check( sol->get_length_of_path( i ) == 2 );
-        mu_check( sol->get_time_path( i ) == 5.0 );
-        mu_check( sol->get_last_path_vertice_in_path( i )->get_hash() == final->get_hash() );
+        mu_check( sol.get_length_of_path( i ) == 2 );
+        mu_check( sol.get_time_path( i ) == 5.0 );
+        mu_check( sol.get_last_path_vertice_in_path( i )->get_hash() == final.get_hash() );
+        mu_check( sol.get_rewards( i ) == 0 );
     }
-	mu_check( sol->get_number_paths() == 2 );
-    mu_check( sol->get_time_per_path() == 7.5 );
-    mu_check( sol->get_total_rewards() == 0.0 );
-    mu_check( sol->get_total_time() == 10.0 );
-    delete initial;
-    delete final;
-    delete sol;
+	mu_check( sol.get_number_paths() == 2 );
+    mu_check( sol.get_time_per_path() == 7.5 );
+    mu_check( sol.get_total_rewards() == 0.0 );
+    mu_check( absolute( sol.get_total_time(), 2 ) == absolute( 10.0, 2 ) );
 }
 
 MU_TEST(solution_set_initial_and_final_vertice_fail) {
-    Vertice * initial = new Vertice( 0.0, 0.0, 0 );
-    Vertice * final = new Vertice( 3.0, 4.0, 0 );
-    Solution * sol = new Solution( 2, 7.5 );
-    bool result = sol->add_initial_and_final_vertice( -1, initial, final );
+    Vertice initial( 0.0, 0.0, 0 );
+    Vertice final( 3.0, 4.0, 0 );
+    Solution sol( 2, 7.5 );
+    bool result = sol.add_initial_and_final_vertice( -1, &initial, &final );
     mu_check( result == false );
-    result = sol->add_initial_and_final_vertice( 0, initial, 0 );
+    result = sol.add_initial_and_final_vertice( 0, &initial, 0 );
     mu_check( result == false );
-    result = sol->add_initial_and_final_vertice( 0, 0, final );
+    result = sol.add_initial_and_final_vertice( 0, 0, &final );
     mu_check( result == false );
-    result = sol->add_initial_and_final_vertice( 0, initial, final );
+    result = sol.add_initial_and_final_vertice( 0, &initial, &final );
     mu_check( result == true );
-    result = sol->add_initial_and_final_vertice( 1, initial, final );
+    result = sol.add_initial_and_final_vertice( 1, &initial, &final );
     mu_check( result == true );
-    result = sol->add_initial_and_final_vertice( 2, initial, final );
+    result = sol.add_initial_and_final_vertice( 2, &initial, &final );
     mu_check( result == false );
-    delete initial;
-    delete final;
-    delete sol;
+    for( int i = 0; i < 2; i++ ){
+        mu_check( sol.get_length_of_path( i ) == 2 );
+        mu_check( sol.get_time_path( i ) == 5.0 );
+        mu_check( sol.get_last_path_vertice_in_path( i )->get_hash() == final.get_hash() );
+        mu_check( sol.get_rewards( i ) == 0 );
+    }
+	mu_check( sol.get_number_paths() == 2 );
+    mu_check( sol.get_time_per_path() == 7.5 );
+    mu_check( sol.get_total_rewards() == 0.0 );
+    mu_check( absolute( sol.get_total_time(), 2 ) == absolute( 10.0, 2 ) );
 }
 
 MU_TEST(solution_add_vertice_in_path) {
-    Vertice * initial = new Vertice( 0.0, 0.0, 0 );
-    Vertice * final = new Vertice( 3.0, 4.0, 0 );
-    Vertice * new_v = new Vertice( 6.0, 8.0, 5 );
-    Solution * sol = new Solution( 2, 7.5 );
+    Vertice initial( 0.0, 0.0, 0 );
+    Vertice final( 3.0, 4.0, 0 );
+    Vertice new_v( 6.0, 8.0, 5 );
+    Solution sol( 2, 7.5 );
     for( int i = 0; i < 2; i++ ){
-        sol->add_initial_and_final_vertice( i, initial, final );
+        sol.add_initial_and_final_vertice( i, &initial, &final );
     }
-    bool result = sol->add( 0, new_v );
+    bool result = sol.add( 0, &new_v );
     mu_check( result == true );
-    mu_check( sol->get_length_of_path( 0 ) == 3 );
-    mu_check( sol->get_time_path( 0 ) == 15.0 );
-    mu_check( sol->get_last_path_vertice_in_path( 0 )->get_hash() == final->get_hash() );
+    mu_check( sol.get_length_of_path( 0 ) == 3 );
+    mu_check( sol.get_time_path( 0 ) == 15.0 );
+    mu_check( sol.get_last_path_vertice_in_path( 0 )->get_hash() == final.get_hash() );
+    mu_check( sol.get_rewards( 0 ) == 5 );
 
-    mu_check( sol->get_length_of_path( 1 ) == 2 );
-    mu_check( sol->get_time_path( 1 ) == 5.0 );
-    mu_check( sol->get_last_path_vertice_in_path( 1 )->get_hash() == final->get_hash() );
+    mu_check( sol.get_length_of_path( 1 ) == 2 );
+    mu_check( sol.get_time_path( 1 ) == 5.0 );
+    mu_check( sol.get_last_path_vertice_in_path( 1 )->get_hash() == final.get_hash() );
+    mu_check( sol.get_rewards( 1 ) == 0 );
 
-	mu_check( sol->get_number_paths() == 2 );
-    mu_check( sol->get_time_per_path() == 7.5 );
-    mu_check( sol->get_total_rewards() == 5.0 );
-    mu_check( sol->get_total_time() == 20.0 );
-    
-    delete initial;
-    delete final;
-    delete new_v;
-    delete sol;
+	mu_check( sol.get_number_paths() == 2 );
+    mu_check( sol.get_time_per_path() == 7.5 );
+    mu_check( sol.get_total_rewards() == 5.0 );
+    mu_check( sol.get_total_time() == 20.0 );
 }
 
 MU_TEST(solution_add_vertice_in_path_fail) {
-    Vertice * initial = new Vertice( 0.0, 0.0, 0 );
-    Vertice * final = new Vertice( 3.0, 4.0, 0 );
-    Vertice * new_v = new Vertice( 6.0, 8.0, 5 );
-    Solution * sol = new Solution( 2, 7.5 );
+    Vertice initial( 0.0, 0.0, 0 );
+    Vertice final( 3.0, 4.0, 0 );
+    Vertice new_v( 6.0, 8.0, 5 );
+    Solution sol( 2, 7.5 );
+    sol.lock_checker();
     for( int i = 0; i < 2; i++ ){
-        sol->add_initial_and_final_vertice( i, initial, final );
+        sol.add_initial_and_final_vertice( i, &initial, &final );
     }
-    bool result = sol->add( 2, new_v );
+    bool result = sol.add( 2, &new_v );// path invalid
     mu_check( result == false );
-    result = sol->add( 2, 0 );
+    result = sol.add( 2, 0 ); // vertice null
     mu_check( result == false );
-    mu_check( sol->get_length_of_path( 0 ) == 2 );
-    mu_check( sol->get_time_path( 0 ) == 5.0 );
-    mu_check( sol->get_last_path_vertice_in_path( 0 )->get_hash() == final->get_hash() );
+    result = sol.add( 1, &new_v ); // time restriction
+    mu_check( result == false );
+    mu_check( sol.get_length_of_path( 0 ) == 2 );
+    mu_check( sol.get_time_path( 0 ) == 5.0 );
+    mu_check( sol.get_last_path_vertice_in_path( 0 )->get_hash() == final.get_hash() );
+    mu_check( sol.get_rewards( 0 ) == 0.0 );
 
-    mu_check( sol->get_length_of_path( 1 ) == 2 );
-    mu_check( sol->get_time_path( 1 ) == 5.0 );
-    mu_check( sol->get_last_path_vertice_in_path( 1 )->get_hash() == final->get_hash() );
+    mu_check( sol.get_length_of_path( 1 ) == 2 );
+    mu_check( sol.get_time_path( 1 ) == 5.0 );
+    mu_check( sol.get_last_path_vertice_in_path( 1 )->get_hash() == final.get_hash() );
+    mu_check( sol.get_rewards( 1 ) == 0.0 );
 
-	mu_check( sol->get_number_paths() == 2 );
-    mu_check( sol->get_time_per_path() == 7.5 );
-    mu_check( sol->get_total_rewards() == 0.0 );
-    mu_check( sol->get_total_time() == 10.0 );
-    
-    delete initial;
-    delete final;
-    delete new_v;
-    delete sol;
+	mu_check( sol.get_number_paths() == 2 );
+    mu_check( sol.get_time_per_path() == 7.5 );
+    mu_check( sol.get_total_rewards() == 0.0 );
+    mu_check( sol.get_total_time() == 10.0 );
 }
 
 MU_TEST(solution_move_sucess){
