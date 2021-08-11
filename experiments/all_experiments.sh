c7301101280f70b670af9154f1ec6f46d1c85bca
Tiago Funk <tiagoff.tf@gmail.com>
2021-08-11 10:48:16 -0300

    Novo experimento

--- experiments/all_experiments.sh
+++ experiments/all_experiments.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-dir=$1
-pfx_log=$2
+dir=./version_02
 
-echo -n "" > "$pfx_log-log1".txt
-echo -n "" > "$pfx_log-log2".txt
-echo -n "" > "$pfx_log-log3".txt
-echo -n "" > "$pfx_log-log4".txt
-echo -n "" > "$pfx_log-log5".txt
-echo -n "" > "$pfx_log-log6".txt
-echo -n "" > "$pfx_log-log7".txt
-echo -n "" > "$pfx_log-log8".txt
+echo -n "" > "$dir/log1".txt
+echo -n "" > "$dir/log2".txt
+echo -n "" > "$dir/log3".txt
+echo -n "" > "$dir/log4".txt
+echo -n "" > "$dir/log5".txt
+echo -n "" > "$dir/log6".txt
+echo -n "" > "$dir/log7".txt
+echo -n "" > "$dir/log8".txt
 
-sh experiment.sh 4 "$pfx_log-log1".txt $dir &
-#sh experiment.sh 4 "$pfx_log-log2".txt $dir &
-#sh experiment.sh 4 "$pfx_log-log3".txt $dir &
-#sh experiment.sh 4 "$pfx_log-log4".txt $dir &
-#sh experiment.sh 4 "$pfx_log-log5".txt $dir &
-#sh experiment.sh 4 "$pfx_log-log6".txt $dir &
-#sh experiment.sh 4 "$pfx_log-log7".txt $dir &
-#sh experiment.sh 4 "$pfx_log-log8".txt $dir &
+sh experiment.sh 4 "$dir/log1".txt &
+sh experiment.sh 4 "$dir/log2".txt &
+sh experiment.sh 4 "$dir/log3".txt &
+sh experiment.sh 4 "$dir/log4".txt &
+sh experiment.sh 4 "$dir/log5".txt &
+sh experiment.sh 4 "$dir/log6".txt &
+sh experiment.sh 4 "$dir/log7".txt &
+sh experiment.sh 4 "$dir/log8".txt &
--- experiments/experiment.sh
+++ experiments/experiment.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
 execute_all(){
-    for file in $3/*
+    for dir in ../instances/*
     do
-        if [ -f "$file" ]
-        then
-            for n in $(seq 1 $1)
-            do
-                echo "$n-$file"
-                echo "$file" >> $2
-                ./../Project/TOP_GRASP_TS_PR 1 1 1 $file --alpha 0.9 --iterations 4500 --path y --margin 1.9 --removeOperator r --removePercentage 0.15 --shuffleOperator e --addOperator b >> $2
-            done
-        fi
+        for file in $dir/*
+        do
+            if [ -f "$file" ]
+            then
+                for n in $(seq 1 $1)
+                do
+                    echo "$n-$file"
+                    echo "$file" >> $2
+                    ./../Project/TOP_GRASP_TS_PR 1 1 1 $file --alpha 0.9 --iterations 4500 --path y --margin 1.9 --removeOperator r --removePercentage 0.15 --shuffleOperator e --addOperator b >> $2
+                done
+            fi
+        done
     done
 }
 
 n=$1
 log=$2
 dir=$3
-
-echo $n
-echo $log
-echo $dir
 
 execute_all $n $log $dir
