4ce9b0b3291837192e7333d8478296c06a61626e
Tiago Funk <tiagoff.tf@gmail.com>
2021-08-25 10:13:11 -0300

    New Experiment

--- experiments/all_experiments.sh
+++ experiments/all_experiments.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-dir=./version_02
+dir=./version_03
+repetions=4
 
 echo -n "" > "$dir/log1".txt
 echo -n "" > "$dir/log2".txt
@@ -11,11 +12,11 @@ echo -n "" > "$dir/log6".txt
 echo -n "" > "$dir/log7".txt
 echo -n "" > "$dir/log8".txt
 
-sh experiment.sh 4 "$dir/log1".txt &
-sh experiment.sh 4 "$dir/log2".txt &
-sh experiment.sh 4 "$dir/log3".txt &
-sh experiment.sh 4 "$dir/log4".txt &
-sh experiment.sh 4 "$dir/log5".txt &
-sh experiment.sh 4 "$dir/log6".txt &
-sh experiment.sh 4 "$dir/log7".txt &
-sh experiment.sh 4 "$dir/log8".txt &
+sh experiment.sh $repetions "$dir/log1".txt &
+sh experiment.sh $repetions "$dir/log2".txt &
+sh experiment.sh $repetions "$dir/log3".txt &
+sh experiment.sh $repetions "$dir/log4".txt &
+sh experiment.sh $repetions "$dir/log5".txt &
+sh experiment.sh $repetions "$dir/log6".txt &
+sh experiment.sh $repetions "$dir/log7".txt &
+sh experiment.sh $repetions "$dir/log8".txt &
--- experiments/experiment.sh
+++ experiments/experiment.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 execute_all(){
-    for dir in ../instances/*
-    do
-        for file in $dir/*
+    #for dir in ../irace/instances/*
+    #do
+        for file in ../irace/instances/*
         do
             if [ -f "$file" ]
             then
                 for n in $(seq 1 $1)
                 do
                     echo "$n-$file"
-                    echo "$file" >> $2
-                    ./../Project/TOP_GRASP_TS_PR 1 1 1 $file --alpha 0.9 --iterations 4500 --path y --margin 1.9 --removeOperator r --removePercentage 0.15 --shuffleOperator e --addOperator b >> $2
+                    #echo "$file" >> $2
+                    ./../Project/TOP_GRASP_TS_PR 1 1 1 $file --alpha 0.9 --iterations 50000 --path y --margin 1.9 --removeOperator r --removePercentage 0.15 --shuffleOperator e --addOperator b >> $2
                 done
             fi
         done
-    done
+    #done
 }
 
 n=$1
